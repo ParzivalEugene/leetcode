3c5729492b86eece3f8c0f1f71e310e1bd174f31
Eugene Michkov <michkov.evgeny@gmail.com>
2024-07-01 17:13:32 +0300

    fix sh

--- rust/utils/add_problem.sh
+++ rust/utils/add_problem.sh
@@ -1,6 +1,10 @@
 cd $(git rev-parse --show-toplevel)/rust/src/
-touch solutions/$1.rs
-touch tests/$1.rs
-echo "pub mod $1;" >> solutions/mod.rs
-echo "pub mod $1;" >> tests/mod.rs
+touch solutions/p$1.rs
+touch tests/p$1.rs
+echo "pub mod p$1;" >> solutions/mod.rs
+echo "pub mod p$1;" >> tests/mod.rs
+echo "#[cfg(test)]
+mod test {
+    use crate::solutions::p$1::Solution;
+}" >> tests/p$1.rs
 cd -
