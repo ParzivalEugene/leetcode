f1d5a171d247b9d74b645c9ec6294b78ff93a1e2
Eugene Michkov <michkov.evgeny@gmail.com>
2024-07-03 21:59:35 +0300

    add cli

---
+++ cli/add_cli.sh
@@ -0,0 +1,2 @@
+alias x="$(git rev-parse --show-toplevel)/cli/target/release/cli"
+echo "Successfully added alias 'x' to run the CLI"
---
+++ cli/src/main.rs
@@ -0,0 +1,63 @@
+use clap::{Parser, Subcommand};
+
+const RUST_PATH: &str = "rust/src";
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    commmand: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    Add { id: u32 },
+}
+
+fn exists(id: &str) -> bool {
+    std::fs::metadata(format!("{}/solutions/p{}.rs", RUST_PATH, id)).is_ok()
+}
+
+fn add_solution(raw_id: u32) {
+    let id = format!("{:0>4}", raw_id);
+    
+    if exists(&id) {
+        eprintln!("Solution for problem {} already exists", id);
+        return;
+    }
+
+    let path = format!("{}/solutions/p{}.rs", RUST_PATH, id);
+    let test_path = format!("{}/tests/p{}.rs", RUST_PATH, id);
+
+    std::fs::write(&path, "pub struct Solution;").unwrap();
+    std::fs::write(
+        &test_path,
+        format!(
+            "#[cfg(test)]
+mod test {{
+    use crate::solutions::p{}::Solution;
+}}",
+            id
+        ),
+    )
+    .unwrap();
+
+    let mut solutions = std::fs::read_to_string(format!("{}/solutions/mod.rs", RUST_PATH)).unwrap();
+    solutions.push_str(&format!("pub mod p{};\n", id));
+    std::fs::write(format!("{}/solutions/mod.rs", RUST_PATH), solutions).unwrap();
+
+    let mut tests = std::fs::read_to_string(format!("{}/tests/mod.rs", RUST_PATH)).unwrap();
+    tests.push_str(&format!("pub mod p{};\n", id));
+    std::fs::write(format!("{}/tests/mod.rs", RUST_PATH), tests).unwrap();
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.commmand {
+        Commands::Add { id } => {
+            add_solution(id);
+        }
+    }
+}
