167955785872111ca46294c60ed7c772302afdfb
Eugene Michkov <michkov.evgeny@gmail.com>
2025-04-05 14:42:04 +0300

    feature: implement palindrome checker in Go

---
+++ go/p0009/solution.go
@@ -0,0 +1,19 @@
+package main
+
+func isPalindrome(num int) bool {
+	if num < 0 {
+		return false
+	}
+	x := num
+	reversed := 0
+	for x != 0 {
+		reversed = 10*reversed + x%10
+		x /= 10
+	}
+	return (reversed == num)
+}
+
+func main() {
+	println(isPalindrome(9999))
+	println(isPalindrome(121))
+}
